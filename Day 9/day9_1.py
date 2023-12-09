97ac2cf3c971620dda59418c4c44052a5b918759
Phil DS <phillipdesousa9@gmail.com>
2023-12-09 20:17:30 +0000

    Day 9 was easy

---
+++ Day 9/day9_1.py
@@ -0,0 +1,20 @@
+with open('Day 9/day9.txt') as file:
+    score = 0
+
+    for line in file:
+        values = [int(v) for v in line.strip().split()]
+        # print(values)
+        sequences = []
+        current = [*values]
+        sequences.append(current)
+        while not all((l == 0 for l in current)):
+            current = [b-a for a,b in zip(current,current[1:])]
+            sequences.append(current)
+
+        nextVal = 0
+        for s in reversed(sequences):
+            nextVal += s[-1]
+
+        score += nextVal
+
+    print(score)
